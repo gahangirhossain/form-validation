248c088e6891bcac9ad28430c1fd0b54c92a11d5
gahangir <mishogahangir@gmail.com>
2022-08-06 00:39:55 +0600

    add:create form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+function App(){
+  const [userInfo,setUserInfo]=useState({
+    userName:'',
+    password:'',
+  })
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[errorInfo,setErrorInfo]=useState({
+    userName:'',
+    password:'',
+  })
+
+
+  const handleSubmit=(evt)=>{
+    evt.preventDefault()
+    const{userName,password}=userInfo
+    // validation here
+
+      if(userName===''){
+        setErrorInfo({
+          ...errorInfo,
+          userName:'please input your valid name'
+        })
+        return
+      }
+
+      if(password===''){
+        setErrorInfo({
+          ...errorInfo,
+          password:'please provide your valid password'
+        })
+        return
+      }
+      // reset all inputs
+      setUserInfo({
+        userName:'',
+        password:''
+      })
+  }
+
+  const handleChange=(evt)=>{
+    setUserInfo({
+      ...userInfo,
+      [evt.target.name]:evt.target.value
+    })
+      setErrorInfo('')
+      console.log(userInfo)
+  }
+
+  const{userName,password}=userInfo
+  return(
+   <>
+   <div className='center'>
+    <h1>Login</h1>
+    <form method='post' onSubmit={handleSubmit}>
+      <div className='txt_field'>
+        {errorInfo.userName && errorInfo.userName}
+        <input type='text'name='userName' onChange={handleChange} value={userName}  />
+        <label>UserName</label>
+      </div>
+      <div className='txt_field'>
+        {errorInfo.password && errorInfo.password}
+        <input type='password'name='password' onChange={handleChange} value={password} />
+        <label>Password</label>
+      </div>
+      <div className='password'>Forget Password ?</div>
+      <input type='submit' value='login'></input>
+      <div className='signup_link'>
+        Not a member ? <a href='#'>Signup</a>
+      </div>
+    </form>
+   </div>
+   </>
+  )
 }
-
-export default App;
+export default App
